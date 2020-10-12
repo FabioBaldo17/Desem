10cdbd8951c15caa929e20bd94d3ed3a4472e3d8
fabioPoli <fabio.baldo251@studenti.polito.it>
2020-10-13 00:00:53 +0200

    lab 02 c++

---
+++ Lab02/src/main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main()
+{
+	Line l1(Point(10,10),Point(40,40), color::blue);
+	Rectangle r1(Point(10,50),Point(40,90), color::purple);
+	Circle c1(Point(10,90),Point(40,140), color::yellow);
+
+	l1.printInfo();
+	r1.printInfo();
+	c1.printInfo();
+
+	return 0;
+}
